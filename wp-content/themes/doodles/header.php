ae25be6e73671713049dd3eb1825a03fd2e71ba3
Si Jobling <simon.jobling@gmail.com>
2015-03-06 17:46:51 +0000

    Simplify mobile navigation with burger icon and top-down overlay

--- wp-content/themes/doodles/header.php
+++ wp-content/themes/doodles/header.php
@@ -27,7 +27,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'doodles' ); ?></button>
+			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Menu', 'doodles' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
